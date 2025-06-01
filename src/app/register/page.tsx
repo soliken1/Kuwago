6ad58890fcf809d948f93b4cc49485e254f58dea
CaeraDenoir11 <monsterbaxter@gmail.com>
2025-06-01 17:07:27 +0800

    updated register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,11 +1,6 @@
-//Now here call layout to be rendered on the route /register, refer to login's page.tsx
-
 import React from "react";
+import RegisterLayout from "@/layout/auth/RegisterLayout";
 
 export default function Register() {
-  return (
-    <div className="w-screen h-screen justify-center items-center">
-      I am Register Screen!
-    </div>
-  );
+  return <RegisterLayout />;
 }
