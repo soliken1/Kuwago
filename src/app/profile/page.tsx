c745124fe0dcf7fd75462fca7fbfb8f4caaba22a
soliken <kennethrex456@gmail.com>
2025-06-02 11:02:20 +0800

    Profile Initial Screen and Dynamic Data Renderings

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ProfileLayout from "@/layout/auth/ProfileLayout";
+export default function Profile() {
+  return <ProfileLayout />;
+}
---
+++ src/layout/auth/ProfileLayout.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function ProfileLayout() {
+  return <div className="w-screen h-screen flex flex-col"></div>;
+}
