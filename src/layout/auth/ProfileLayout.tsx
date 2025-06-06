db93f63bc09e0dafac81e31000b3481d3e419033
CaeraDenoir11 <monsterbaxter@gmail.com>
2025-06-06 16:44:23 +0800

    reiteration v5

--- src/layout/auth/ProfileLayout.tsx
+++ src/layout/auth/ProfileLayout.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import Navbar from "@/components/dashboard/client/Navbar";
 import ProfileForm from "@/components/profile/client/ProfileForm";
-export default function ProfileLayout() {
-  return (
-    <div className="w-screen h-screen flex flex-col">
-      <Navbar />
-      <ProfileForm />
-    </div>
-  );
+
+interface ProfileLayoutProps {
+  children: ReactNode;
+}
+
+export default function ProfileLayout({ children }: ProfileLayoutProps) {
+  return <div className="min-h-screen bg-gray-50">{children}</div>;
 }
