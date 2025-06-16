4bdab680c473d22d9adbe3d57595f0301b3c989a
soliken <kennethrex456@gmail.com>
2025-06-16 16:13:08 +0800

    Disable Unused Expression ESLint Error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,7 @@ const eslintConfig = [
       "@next/next/no-page-custom-font": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
     },
   }),
 ];
