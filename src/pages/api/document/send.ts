936d29b467164b991402d0a2db97fc99660a150f
soliken <kennethrex456@gmail.com>
2025-06-19 11:53:01 +0800

    Embedding and Sending Emails Handling (Resolved) #16

--- src/pages/api/document/send.ts
+++ src/pages/api/document/send.ts
@@ -15,7 +15,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "GET") {
+  if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
