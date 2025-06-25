f2135d7e9a75d24c8abf08e3b2a98c9785b0d177
soliken <kennethrex456@gmail.com>
2025-06-25 11:08:55 +0800

    Better Notification Scheme (1/2) and FAQ Layout

--- src/components/messaging/client/UserListChat.tsx
+++ src/components/messaging/client/UserListChat.tsx
@@ -15,19 +15,12 @@ import { chatClient } from "@/utils/streamClient";
 import axios from "axios";
 import "stream-chat-react/dist/css/v2/index.css";
 import { BiMessage, BiVideo } from "react-icons/bi";
+import X from "../../../../assets/actions/X";
 
 interface StoredUser {
   uid: string;
   fullName: string;
   profilePicture?: string;
-}
-
-interface ChannelMember {
-  user: {
-    id: string;
-    name: string;
-    image?: string;
-  };
 }
 
 export default function UserListChat() {
@@ -42,6 +35,10 @@ export default function UserListChat() {
     name: string;
   } | null>(null);
   const [hasUnreadMessages, setHasUnreadMessages] = useState(false);
+  const [unreadByChannel, setUnreadByChannel] = useState<
+    Record<string, number>
+  >({});
+  const [showHelpChat, setShowHelpChat] = useState(false);
 
   useEffect(() => {
     const raw = localStorage.getItem("user");
@@ -78,7 +75,14 @@ export default function UserListChat() {
             event.type === "message.new" &&
             event.message?.user?.id !== storedUser.uid
           ) {
-            setHasUnreadMessages(true);
+            const channelId = event.channel?.id;
+            if (channelId) {
+              setUnreadByChannel((prev) => ({
+                ...prev,
+                [channelId]: (prev[channelId] || 0) + 1,
+              }));
+              setHasUnreadMessages(true);
+            }
           }
         });
 
@@ -92,6 +96,19 @@ export default function UserListChat() {
           filters,
           { last_message_at: -1 },
           { watch: true, state: true, message_limit: 1 }
+        );
+
+        const unreadMap: Record<string, number> = {};
+        channels.forEach((channel) => {
+          const unreadCount = channel.countUnread();
+          if (unreadCount > 0) {
+            unreadMap[channel.id!] = unreadCount;
+          }
+        });
+
+        setUnreadByChannel(unreadMap);
+        setHasUnreadMessages(
+          Object.values(unreadMap).some((count) => count > 0)
         );
 
         setChannels(channels);
@@ -110,6 +127,11 @@ export default function UserListChat() {
       }
     };
   }, [storedUser]);
+
+  useEffect(() => {
+    const hasUnread = Object.values(unreadByChannel).some((count) => count > 0);
+    setHasUnreadMessages(hasUnread);
+  }, [unreadByChannel]);
 
   const startVideoCall = async () => {
     if (!activeChannel || !storedUser) return;
@@ -138,21 +160,24 @@ export default function UserListChat() {
   };
 
   useEffect(() => {
-    if (showChat) {
-      setHasUnreadMessages(false);
-    }
-  }, [showChat]);
+    if (!storedUser) return;
 
-  useEffect(() => {
     const audio = new Audio("/sounds/notification.mp3");
 
     const handleNewMessage = (event: any) => {
       if (
         event.type === "message.new" &&
-        event.message?.user?.id !== storedUser?.uid
+        event.message?.user?.id !== storedUser.uid
       ) {
-        setHasUnreadMessages(true);
-        audio.play();
+        const channelId = event.channel?.id;
+        if (channelId) {
+          setUnreadByChannel((prev) => ({
+            ...prev,
+            [channelId]: (prev[channelId] || 0) + 1,
+          }));
+          setHasUnreadMessages(true);
+          audio.play();
+        }
       }
     };
 
@@ -186,124 +211,171 @@ export default function UserListChat() {
 
       {showChat && (
         <div className="fixed inset-0 bg-opacity-50 flex justify-end items-center pe-16 z-50 bg-black/40">
-          <div className="bg-white w-11/12 md:w-3/4 lg:w-1/2 h-4/5 rounded-lg shadow-lg relative flex flex-col">
-            <div className="flex justify-between items-center p-4 border-b border-gray-200">
-              <h2 className="font-bold text-xl">Messages</h2>
-              <button
-                onClick={() => setShowChat(false)}
-                className="p-2 text-gray-600 hover:text-gray-900 rounded-full hover:bg-gray-100"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div className="flex flex-1 overflow-hidden">
-              <div className="w-1/3 border-r border-gray-200 overflow-auto">
-                <Chat client={chatClient}>
-                  <ChannelList
-                    filters={{
-                      type: "messaging",
-                      members: { $in: [storedUser?.uid || ""] },
-                      last_message_at: { $exists: true }, // Excludes channels with no messages
-                    }}
-                    sort={{ last_message_at: -1 }}
-                    options={{ watch: true, state: true, message_limit: 1 }}
-                    Preview={(props) => {
-                      const { channel } = props;
-                      const members = Object.values(channel.state.members)
-                        .filter(
-                          (member: any) => member.user.id !== storedUser?.uid
-                        )
-                        .map((member: any) => member.user);
-                      const member = members[0];
-
-                      const lastMessage =
-                        channel.state.messages[
-                          channel.state.messages.length - 1
-                        ];
-                      const lastMessageText =
-                        lastMessage?.text || "No messages yet";
-                      const lastMessageDate = lastMessage?.created_at;
-
-                      const formatDate = (dateString: string | Date) => {
-                        if (!dateString) return "";
-                        const date = new Date(dateString);
-                        return date.toLocaleTimeString([], {
-                          hour: "2-digit",
-                          minute: "2-digit",
-                        });
-                      };
-
-                      return (
-                        <div
-                          className={`flex items-center gap-3 p-3 hover:bg-gray-100 cursor-pointer ${
-                            activeChannel?.id === channel.id
-                              ? "bg-gray-100"
-                              : ""
-                          }`}
-                          onClick={() => setActiveChannel(channel)}
-                        >
-                          <Avatar image={member?.image} name={member?.name} />
-                          <div className="flex-1 min-w-0">
-                            <div className="flex justify-between items-center">
-                              <p className="font-semibold truncate max-w-28">
-                                {member?.name}
-                              </p>
-                              <span className="text-xs text-gray-500">
-                                {formatDate(lastMessageDate)}
-                              </span>
-                            </div>
-                            <p className="text-sm text-gray-500 truncate">
-                              {lastMessageText}
-                            </p>
-                          </div>
-                        </div>
-                      );
-                    }}
-                  />
-                </Chat>
-              </div>
-
-              <div className="flex-1 flex flex-col pb-4">
-                {activeChannel ? (
-                  <Chat client={chatClient} theme="messaging light">
-                    <Channel channel={activeChannel}>
-                      <Window>
-                        <div className="flex justify-between items-center p-2 border-b border-gray-200">
-                          <ChannelHeader />
-                          <button
-                            onClick={startVideoCall}
-                            className="flex items-center gap-2 px-4 py-2 rounded-full bg-blue-500 text-white hover:bg-blue-600"
-                            title="Start Video Call"
-                          >
-                            <BiVideo size={20} />
-                            <span>Call</span>
-                          </button>
-                        </div>
-                        <MessageList />
-                        <MessageInput />
-                      </Window>
-                    </Channel>
-                  </Chat>
-                ) : (
-                  <div className="flex items-center justify-center flex-1">
-                    Select a chat to start messaging
+          <div className="bg-white w-11/12 md:w-3/4 h-4/5 rounded-lg shadow-lg relative flex flex-col">
+            {!showHelpChat ? (
+              <>
+                <div className="flex justify-between items-center p-4 border-b border-gray-200">
+                  <h2 className="font-bold text-xl">Messages</h2>
+                  <div className="flex gap-5">
+                    <button
+                      onClick={() => {
+                        setShowHelpChat(true);
+                      }}
+                      className="border border-gray-400 rounded-xl px-4"
+                    >
+                      Need Help?
+                    </button>
+                    <button
+                      onClick={() => setShowChat(false)}
+                      className="p-2 text-gray-600 hover:text-gray-900 rounded-full hover:bg-gray-100"
+                    >
+                      <X />
+                    </button>
                   </div>
-                )}
-              </div>
-            </div>
+                </div>
+                <div className="flex flex-1 overflow-hidden">
+                  <div className="w-1/3 border-r border-gray-200 overflow-auto">
+                    <Chat client={chatClient}>
+                      <ChannelList
+                        filters={{
+                          type: "messaging",
+                          members: { $in: [storedUser?.uid || ""] },
+                          last_message_at: { $exists: true }, // Excludes channels with no messages
+                        }}
+                        sort={{ last_message_at: -1 }}
+                        options={{ watch: true, state: true, message_limit: 1 }}
+                        Preview={(props) => {
+                          const { channel } = props;
+                          const members = Object.values(channel.state.members)
+                            .filter(
+                              (member: any) =>
+                                member.user.id !== storedUser?.uid
+                            )
+                            .map((member: any) => member.user);
+                          const member = members[0];
+
+                          const lastMessage =
+                            channel.state.messages[
+                              channel.state.messages.length - 1
+                            ];
+                          const lastMessageText =
+                            lastMessage?.text || "No messages yet";
+                          const lastMessageDate = lastMessage?.created_at;
+
+                          const formatDate = (dateString: string | Date) => {
+                            if (!dateString) return "";
+                            const date = new Date(dateString);
+                            return date.toLocaleTimeString([], {
+                              hour: "2-digit",
+                              minute: "2-digit",
+                            });
+                          };
+
+                          return (
+                            <div
+                              className={`flex items-center gap-3 p-3 hover:bg-gray-100 cursor-pointer ${
+                                activeChannel?.id === channel.id
+                                  ? "bg-gray-100"
+                                  : ""
+                              }`}
+                              onClick={async () => {
+                                setActiveChannel(channel);
+                                await channel.markRead();
+                                setUnreadByChannel((prev) => ({
+                                  ...prev,
+                                  [channel.id!]: 0,
+                                }));
+                              }}
+                            >
+                              <Avatar
+                                image={member?.image}
+                                name={member?.name}
+                              />
+                              <div className="flex-1 min-w-0">
+                                <div className="flex justify-between items-center">
+                                  <p className="font-semibold truncate">
+                                    {member?.name}
+                                  </p>
+
+                                  <div className="flex gap-2">
+                                    {channel.id &&
+                                      unreadByChannel[channel.id] > 0 && (
+                                        <span className="ml-2 inline-block bg-red-500 text-white text-xs px-2 py-0.5 rounded-full">
+                                          {unreadByChannel[channel.id]}
+                                        </span>
+                                      )}
+
+                                    <span className="text-xs text-gray-500">
+                                      {formatDate(lastMessageDate)}
+                                    </span>
+                                  </div>
+                                </div>
+                                <p className="text-sm text-gray-500 truncate">
+                                  {lastMessageText}
+                                </p>
+                              </div>
+                            </div>
+                          );
+                        }}
+                      />
+                    </Chat>
+                  </div>
+
+                  <div className="flex-1 flex flex-col pb-4">
+                    {activeChannel ? (
+                      <Chat client={chatClient} theme="messaging light">
+                        <Channel channel={activeChannel}>
+                          <Window>
+                            <div className="flex justify-between items-center p-2 border-b border-gray-200">
+                              <ChannelHeader />
+                              <button
+                                onClick={startVideoCall}
+                                className="flex items-center gap-2 px-4 py-2 rounded-full bg-blue-500 text-white hover:bg-blue-600"
+                                title="Start Video Call"
+                              >
+                                <BiVideo size={20} />
+                                <span>Call</span>
+                              </button>
+                            </div>
+                            <MessageList />
+                            <MessageInput />
+                          </Window>
+                        </Channel>
+                      </Chat>
+                    ) : (
+                      <div className="flex items-center justify-center flex-1">
+                        Select a chat to start messaging
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="flex justify-between items-center p-4 border-b border-gray-200">
+                  <h2 className="font-bold text-xl">Help Chat</h2>
+                  <div className="flex gap-5">
+                    <button
+                      onClick={() => {
+                        setShowHelpChat(false);
+                      }}
+                      className="border border-gray-400 rounded-xl px-4"
+                    >
+                      Go Back
+                    </button>
+                    <button
+                      onClick={() => setShowChat(false)}
+                      className="p-2 text-gray-600 hover:text-gray-900 rounded-full hover:bg-gray-100"
+                    >
+                      <X />
+                    </button>
+                  </div>
+                </div>
+                <div className="flex flex-1 font-bold justify-center items-center overflow-hidden">
+                  I'm Under Construction...
+                </div>
+              </>
+            )}
           </div>
         </div>
       )}
