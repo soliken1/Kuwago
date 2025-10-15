6725c9f3f603e43cd1fa683e9236cddbbc2a6950
Scyn4pse <canquechiara76@gmail.com>
2025-10-15 23:16:42 +0800

    Approved Loans Revamp

--- src/layout/approvedloans/ApprovedLoansLayout.tsx
+++ src/layout/approvedloans/ApprovedLoansLayout.tsx
@@ -1,12 +1,27 @@
+"use client";
 import React from "react";
-import Navbar from "@/components/dashboard/client/Navbar";
+import NewSidebar from "@/components/dashboard/client/NewSidebar";
+import ApprovedLoansHeader from "@/components/approvedloans/client/ApprovedLoansHeader";
 import ApprovedComponent from "@/components/approvedloans/client/ApprovedComponent";
 
-export default function DashboardLayout() {
+export default function ApprovedLoansLayout() {
   return (
-    <div className="w-screen h-screen flex flex-col">
-      <Navbar />
-      <ApprovedComponent />
+    <div className="w-full h-screen flex bg-gray-50 relative">
+      {/* Sidebar */}
+      <div className="w-80 flex-shrink-0">
+        <NewSidebar />
+      </div>
+      
+      {/* Main Content */}
+      <div className="flex-1 flex flex-col min-w-0">
+        {/* Header */}
+        <ApprovedLoansHeader />
+        
+        {/* Content Area */}
+        <div className="flex-1 p-8 overflow-auto">
+          <ApprovedComponent />
+        </div>
+      </div>
     </div>
   );
 }
