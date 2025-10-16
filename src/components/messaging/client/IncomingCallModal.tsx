17523b78c779092e177b140d8db576499be4f609
Scyn4pse <canquechiara76@gmail.com>
2025-10-16 09:16:17 +0800

    incoming call fix

--- src/components/messaging/client/IncomingCallModal.tsx
+++ src/components/messaging/client/IncomingCallModal.tsx
@@ -33,8 +33,8 @@ const IncomingCallModal: React.FC<IncomingCallModalProps> = ({
             onClick={onAccept}
             className="text-white px-4 py-2 rounded-lg"
             style={{ backgroundColor: '#85d4a4' }}
-            onMouseEnter={(e) => e.target.style.backgroundColor = '#6bc48a'}
-            onMouseLeave={(e) => e.target.style.backgroundColor = '#85d4a4'}
+            onMouseEnter={(e) => (e.target as HTMLButtonElement).style.backgroundColor = '#6bc48a'}
+            onMouseLeave={(e) => (e.target as HTMLButtonElement).style.backgroundColor = '#85d4a4'}
           >
             Accept
           </button>
