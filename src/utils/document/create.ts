b136231581e77ce0be06b744f90c321ff668a427
soliken <kennethrex456@gmail.com>
2025-11-01 23:45:10 +0800

    feature: update new pandadoc credentials [skip]

--- src/utils/document/create.ts
+++ src/utils/document/create.ts
@@ -1,12 +1,11 @@
 import axios from "axios";
-import sendDocumentLink from "@/utils/document/send";
 
 const createDocument = async (selectedLoan: any, storedUser: any) => {
   const apiKey = process.env.PANDADOC_API_KEY;
   const templateId = process.env.PANDADOC_TEMPLATE_ID;
 
   const payload = {
-    name: "Loan Approval Test Document",
+    name: "Loan Approval Document",
     template_uuid: templateId,
     recipients: [
       {
@@ -29,24 +28,12 @@ const createDocument = async (selectedLoan: any, storedUser: any) => {
     send_email: true,
   };
 
-  const response = await axios.post(
-    "https://api.pandadoc.com/public/v1/documents",
-    payload,
-    {
-      headers: {
-        Authorization: `API-Key ${apiKey}`,
-        "Content-Type": "application/json",
-      },
-    }
-  );
-
-  const url = `https://app.pandadoc.com/a/#/documents/${response.data.uuid}`;
-  await sendDocumentLink(
-    storedUser.email,
-    storedUser.fullname,
-    url,
-    `Generated Document For ${selectedLoan.loanInfo.firstName} ${selectedLoan.loanInfo.lastName}`
-  );
+  await axios.post("https://api.pandadoc.com/public/v1/documents", payload, {
+    headers: {
+      Authorization: `API-Key ${apiKey}`,
+      "Content-Type": "application/json",
+    },
+  });
 };
 
 export default createDocument;
